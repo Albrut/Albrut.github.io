1059d3e5b01125bdab18d02d02fbe40051fc26df
osolenye <apasniynegr228@gmail.com>
2024-04-27 10:46:26 +0600

    searching for car insurance payment request

---
+++ js/car_insurance_request.js
@@ -0,0 +1,67 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Предотвращаем отправку формы по умолчанию
+
+    // Получаем данные из формы
+    const place = document.getElementById('up').value;
+    const photo = document.getElementById('paper').value; // Ваш файл для загрузки
+    const file = document.querySelector('.input_file').files[0]; // Получаем файл из input
+
+    // Создаем объект JSON
+    const data = {
+        "accidentPlace": place,
+        "image": file, // Если нужно, вы можете добавить путь к файлу, но это может быть небезопасно
+        "policy_num": 1
+    };
+
+    // Отправляем данные на сервер
+    sendDataToServer(data);
+});
+
+function sendDataToServer(data) {
+    // Преобразуем объект JSON в строку
+    const jsonData = JSON.stringify(data);
+
+    // Отправляем данные на сервер
+    fetch('http://35.192.170.245:8000/api/payment/cargo/request/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'multipart-form/data',
+            "Authorization": "Bearer " + localStorage.getItem("accessToken")
+        },
+        body: jsonData
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Network response was not ok.');
+        })
+        .then(data => {
+            console.log(data); // Здесь вы можете обработать ответ от сервера
+        })
+        .catch(error => {
+            console.error('There was a problem with your fetch operation:', error);
+        });
+}
+
+
+var tariff_chooser_button_click = document.getElementById("tariff_chooser_button_click");
+var tariffs = document.getElementById("tariffs");
+var tariff_opened = false;
+tariff_chooser_button_click.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (tariff_opened == false) {
+        tariffs.style.display = "flex";
+        arrow_down.style.display = "none";
+        arrow_up.style.display = "block";
+        tariff_opened = true;
+    } else {
+        tariffs.style.display = "none";
+        arrow_up.style.display = "none";
+        arrow_down.style.display = "block";
+        tariff_opened = false;
+    }
+});
