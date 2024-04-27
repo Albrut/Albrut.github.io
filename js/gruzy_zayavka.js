c7b60ab5d4ade2217c451434e4356e3b6191daed
osolenye <apasniynegr228@gmail.com>
2024-04-27 08:53:52 +0600

    searching for car insurance payment request

--- js/gruzy_zayavka.js
+++ js/gruzy_zayavka.js
@@ -45,3 +45,23 @@ function sendDataToServer(data) {
             console.error('There was a problem with your fetch operation:', error);
         });
 }
+
+
+var tariff_chooser_button_click = document.getElementById("tariff_chooser_button_click");
+var tariffs = document.getElementById("tariffs");
+var tariff_opened = false;
+tariff_chooser_button_click.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (tariff_opened == false) {
+        tariffs.style.display = "flex";
+        arrow_down.style.display = "none";
+        arrow_up.style.display = "block";
+        tariff_opened = true;
+    } else {
+        tariffs.style.display = "none";
+        arrow_up.style.display = "none";
+        arrow_down.style.display = "block";
+        tariff_opened = false;
+    }
+});
